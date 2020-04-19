aea6549d60ac36c51bf95dd4218fc61ef505112e
Zachary Layne Thoreson <zthoreso@purdue.edu>
2020-04-19 19:15:01 -0400

    Embed changes

--- rallybot.py
+++ rallybot.py
@@ -31,6 +31,7 @@ async def on_message(message):
         embed=discord.Embed(title='Help Menu', description="Current commands supported by the bot", color=0xeee657)
         embed.add_field(name="!builds_support", description="Prints the current EK Support Builds", inline=False)
         embed.add_field(name="!builds_dps", description="Prints the current EK DPS Builds", inline=False)
+        await message.channel.send(embed=embed)
 
     if message.content == '!builds_dps':
         embed=discord.Embed(title="DPS Builds ", description="Current EK meta DPS builds ")
@@ -39,14 +40,14 @@ async def on_message(message):
         embed.add_field(name="Dragon Hunter", value="http://gw2skills.net/editor/?PWiAENlFwwYdMHWJO0X3tVA-zRJYiRFfh0SEkZJUbCo0hIANL8fGB-e", inline=True)
         embed.add_field(name="Scrapper (Bomb)", value="https://metabattle.com/wiki/Build:Scrapper_-_Big_Bomb_Kit ", inline=True)
         embed.add_field(name="Renegade", value="http://en.gw2skills.net/editor/?PmiAIZldQIMHKi1QSsHCi9RgsASgFzU57G-zRJYnRF/ZkpUIdVgGvEhpHA-e ", inline=True)
-        await self.bot.say(embed=embed)
+        await message.channel.send(embed=embed)
 
     if message.content == '!builds_support':
         embed=discord.Embed(title="Support Builds", description="Current EK meta support builds", color=0x00ff00)
         embed.add_field(name="Firebrand", value="http://en.gw2skills.net/editor/?PWyAYl7lRwwYdMJmJmyXqvdA-zRJYjRNfZkZKUdF47hIANLtfAA-e", inline=False)
         embed.add_field(name="Scapper (Medic)", value="http://en.gw2skills.net/editor/?Pe0AIp7lNwcYPMP2Je6TntKA-zRJYjRN/ZkpSoaZgknl2PAA-e", inline=False)
         embed.add_field(name="Tempest", value=" http://gw2skills.net/editor/?PG0AgiZlRwSYNMGWJm2WrtdA-zVhYBRQIEdwYYnBfIBJByUF0tQwhXig0bp9DA-w", inline=Frue)
-        await self.bot.say(embed=embed)
+        await message.channel.send(embed=embed)
 
 
 client.run(TOKEN)
