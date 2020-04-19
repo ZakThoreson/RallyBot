22a93f3794299522983cdf5ffcad0818bc5c3517
Zachary Layne Thoreson <zthoreso@purdue.edu>
2020-04-19 19:17:39 -0400

    Formatting of builds

--- rallybot.py
+++ rallybot.py
@@ -35,18 +35,18 @@ async def on_message(message):
 
     if message.content == '!builds_dps':
         embed=discord.Embed(title="DPS Builds ", description="Current EK meta DPS builds ")
-        embed.add_field(name="Reaper (Power)", value="http://en.gw2skills.net/editor/?PS0AEd3lVwOYIsEWJeeXntbA-zRRYBRNnvYQBFSThQmVQFJgKDA-e ", inline=True)
-        embed.add_field(name="Guardian (Burn)", value="http://en.gw2skills.net/editor/?PWwAYt/lFwQCbdstC3IO+KZrNA-zVRYcRNHPZQDjRiKoeK44iEE9W45TD-w ", inline=True)
-        embed.add_field(name="Dragon Hunter", value="http://gw2skills.net/editor/?PWiAENlFwwYdMHWJO0X3tVA-zRJYiRFfh0SEkZJUbCo0hIANL8fGB-e", inline=True)
-        embed.add_field(name="Scrapper (Bomb)", value="https://metabattle.com/wiki/Build:Scrapper_-_Big_Bomb_Kit ", inline=True)
-        embed.add_field(name="Renegade", value="http://en.gw2skills.net/editor/?PmiAIZldQIMHKi1QSsHCi9RgsASgFzU57G-zRJYnRF/ZkpUIdVgGvEhpHA-e ", inline=True)
+        embed.add_field(name="Reaper (Power)", value="http://en.gw2skills.net/editor/?PS0AEd3lVwOYIsEWJeeXntbA-zRRYBRNnvYQBFSThQmVQFJgKDA-e ", inline=False)
+        embed.add_field(name="Guardian (Burn)", value="http://en.gw2skills.net/editor/?PWwAYt/lFwQCbdstC3IO+KZrNA-zVRYcRNHPZQDjRiKoeK44iEE9W45TD-w ", inline=False)
+        embed.add_field(name="Dragon Hunter", value="http://gw2skills.net/editor/?PWiAENlFwwYdMHWJO0X3tVA-zRJYiRFfh0SEkZJUbCo0hIANL8fGB-e", inline=False)
+        embed.add_field(name="Scrapper (Bomb)", value="https://metabattle.com/wiki/Build:Scrapper_-_Big_Bomb_Kit ", inline=False)
+        embed.add_field(name="Renegade", value="http://en.gw2skills.net/editor/?PmiAIZldQIMHKi1QSsHCi9RgsASgFzU57G-zRJYnRF/ZkpUIdVgGvEhpHA-e ", inline=False)
         await message.channel.send(embed=embed)
 
     if message.content == '!builds_support':
         embed=discord.Embed(title="Support Builds", description="Current EK meta support builds", color=0x00ff00)
         embed.add_field(name="Firebrand", value="http://en.gw2skills.net/editor/?PWyAYl7lRwwYdMJmJmyXqvdA-zRJYjRNfZkZKUdF47hIANLtfAA-e", inline=False)
         embed.add_field(name="Scapper (Medic)", value="http://en.gw2skills.net/editor/?Pe0AIp7lNwcYPMP2Je6TntKA-zRJYjRN/ZkpSoaZgknl2PAA-e", inline=False)
-        embed.add_field(name="Tempest", value=" http://gw2skills.net/editor/?PG0AgiZlRwSYNMGWJm2WrtdA-zVhYBRQIEdwYYnBfIBJByUF0tQwhXig0bp9DA-w", inline=Frue)
+        embed.add_field(name="Tempest", value=" http://gw2skills.net/editor/?PG0AgiZlRwSYNMGWJm2WrtdA-zVhYBRQIEdwYYnBfIBJByUF0tQwhXig0bp9DA-w", inline=False)
         await message.channel.send(embed=embed)
 
 
